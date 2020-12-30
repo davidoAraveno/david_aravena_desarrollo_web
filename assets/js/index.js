7f437f51249e34deaf123073fb722ae5374fb163
david aravena <david.aravenap@gmail.com>
2020-12-30 12:05:24 -0300

    solo header y navbar

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+////SMOOTH SCROOL//////
+
+$(function(){
+    $("a").click(function(event){
+        if(this.hash !== ""){
+            event.preventDefault();
+            var id = this.hash;
+
+            $("html,body").animate({
+                scrollTop: $(id).offset().top
+            }, 800, function(){
+                window.location.hash = id;
+            });
+        }
+    })
+})
+
+
+
+//////MODAL////////
+
+(function(){
+
+    $(function(){
+        $('#exampleModal').modal()
+    })
+}());
+
+
+
+
+
+/////tooltip///////
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
